1ee516b9aa80e9a3f1b71a7d6502c909d4a4eaaa
Junyeol_Lee <alltheway544@gmail.com>
2020-02-20 02:43:24 -0600

    added validation to the models

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Pokemon < ApplicationRecord
-  belongs_to :request
+  belongs_to :request # One to many association
+
+  validates :name, presence: true
+  # The FK is also required.
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Request < ApplicationRecord
+  has_many :pokemons
+  validates :date, :serviceArea, :serviceRequest, :location, presence: true
 end
