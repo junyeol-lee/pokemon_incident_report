d1b556ef1b93cb58dae190562eb2906d126536bf
Junyeol_Lee <alltheway544@gmail.com>
2020-02-23 00:00:23 -0600

    requests controller has been created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'pokemonnests/index'
-  get 'pokemonnests/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
